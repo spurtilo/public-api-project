619b082afdec7eb6f206cfff042a5447d8f03793
Simo Purtilo <148346961+spurtilo@users.noreply.github.com>
2023-12-13 14:24:11 +0200

    Initial commit

---
+++ index.js
@@ -0,0 +1,58 @@
+import express from 'express';
+import axios from 'axios';
+import bodyParser from "body-parser";
+
+const app = express();
+const port = 3000;
+const API_URL = 'https://v2.jokeapi.dev/joke/Any?idRange=';
+
+function userNameAsNumber(name) {
+    const letterValues = {
+        'a': 1, 'b': 2, 'c': 3, 'd': 4, 'e': 5, 'f': 6, 'g': 7, 'h': 8, 'i': 9,
+        'j': 10, 'k': 11, 'l': 12, 'm': 13, 'n': 14, 'o': 15, 'p': 16, 'q': 17,
+        'r': 18, 's': 19, 't': 20, 'u': 21, 'v': 22, 'w': 23, 'x': 24, 'y': 25, 'z': 26,
+        'å': 27, 'ä': 28, 'ö': 29
+      };
+    const nameArray = name.split('');
+    let result = 0;
+    
+    nameArray.forEach(letter => {
+        if (letter in letterValues) {
+            result += letterValues[letter];
+        }
+    });
+    return result;
+}
+
+app.use(express.static('public'));
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.get('/', (req, res) => {
+    res.render('index.ejs');
+});
+
+
+app.post('/submit', async (req, res) => {
+    try {
+        const fullName = `${req.body.firstName}${req.body.lastName}`.toLowerCase()
+        const numberForName = userNameAsNumber(fullName);
+    
+        const result = await axios.get(API_URL + numberForName);
+        res.render('index.ejs', {
+            jokeSetup: result.data.setup,
+            jokeDelivery: result.data.delivery
+        });
+    
+        console.log(fullName)
+        console.log(numberForName);
+        console.log(result.data.setup, result.data.delivery)
+        res.sendStatus(200);
+    } catch (error) {
+        console.log(error.message)
+        res.status(500);
+    }
+});
+
+app.listen(port, () => {
+    console.log(`Server is running on port ${port}`);
+});
