a98fa2f4041dd0e5d4d71e36cd173588e7a81c74
Simo Purtilo <148346961+spurtilo@users.noreply.github.com>
2023-12-14 15:45:30 +0200

    Improve userNameAsNumber function by changing the conversion algorithm to use Unicode points.

--- index.js
+++ index.js
@@ -7,21 +7,13 @@ const port = 3000;
 const API_URL = 'https://v2.jokeapi.dev/joke/Any?idRange=';
 
 function userNameAsNumber(name) {
-    const letterValues = {
-        'a': 1, 'b': 2, 'c': 3, 'd': 4, 'e': 5, 'f': 6, 'g': 7, 'h': 8, 'i': 9,
-        'j': 10, 'k': 11, 'l': 12, 'm': 13, 'n': 14, 'o': 15, 'p': 16, 'q': 17,
-        'r': 18, 's': 19, 't': 20, 'u': 21, 'v': 22, 'w': 23, 'x': 24, 'y': 25, 'z': 26,
-        'å': 27, 'ä': 28, 'ö': 29
-      };
-    const nameArray = name.split('');
+    name = name.toLowerCase()
     let result = 0;
     
-    nameArray.forEach(letter => {
-        if (letter in letterValues) {
-            result += letterValues[letter];
-        }
-    });
-    return result;
+    for (let i = 0; i < name.length; i++) {
+        result += name.charCodeAt(i);
+    }
+    return result % 310;
 }
 
 function findSafeJoke(jokes) {
@@ -42,17 +34,18 @@ app.get('/', (req, res) => {
 
 app.post('/submit', async (req, res) => {
     try {
-        const fullName = `${req.body.firstName}${req.body.lastName}`.toLowerCase()
+        const fullName = `${req.body.firstName}${req.body.lastName}`;
         const firstID = userNameAsNumber(fullName);
-        const lastID = firstID + 9
+        console.log(firstID)
+        const lastID = firstID + 9;
+        console.log(API_URL + firstID + '-' + lastID + '&amount=10')
     
         const result = await axios.get(API_URL + firstID + '-' + lastID + '&amount=10');
         const jokeToDisplay = findSafeJoke(result.data.jokes);
-        console.log(jokeToDisplay);
         res.render('index.ejs', {
             joke: jokeToDisplay
         });
-        
+
         res.sendStatus(200);
     } catch (error) {
         console.log(error.message)
